a51b2ab3f942a86443e7595dc9d67e1552e1195b
Marko Mijacevic <marko0905@gmail.com>
2024-10-03 12:41:55 +0200

    03/10/24 - 12:41

--- lua/marko/core/keymaps.lua
+++ lua/marko/core/keymaps.lua
@@ -2,11 +2,11 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
 
 -- increment/decrement numbers (uncomment to enable)
--- keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
--- keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+--[[ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement ]]
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -14,11 +14,21 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+--[[ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab ]]
+
+-- terminal
+keymap.set("n", "<leader>tv", function()
+  vim.cmd("vsplit | terminal") -- Open terminal in a vertical split
+  vim.api.nvim_command("startinsert") -- Automatically enter insert mode
+end, { desc = "Open terminal verticaly" }) -- open terminal
+keymap.set("n", "<leader>th", function()
+  vim.cmd("split | terminal") -- Open terminal in a horizontal split
+  vim.api.nvim_command("startinsert") -- Automatically enter insert mode
+end, { desc = "Open terminal horizontaly" }) -- open terminal
 
 -- split movement
 keymap.set("n", "<C-M-h>", "<C-w>h", { desc = "Move to left buffer" }) -- move to left split
